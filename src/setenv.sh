b13841b8f2e27f0ad9b1c02639fa41d1e1d6e2f4
blockzhou <54791381+blockzhou@users.noreply.github.com>
2019-12-07 13:52:32 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,8 +1,8 @@
 export GITACCOUNT=blockzhou
 
 export MYCLUSTER=tektonknativebeijing33
-export KUBECONFIG=
+export KUBECONFIG=/tmp/ic/AB/.bluemix/plugins/container-service/clusters/tektonknativebeijing33/kube-config-tok02-tektonknativebeijing33.yml
 
 export NAMESPACE=zhou-cluster
 export EMAIL=1436129831@qq.com
-export INGRESS=
+export INGRESS=tektonknativebeijing33-0e3e0ef4c9c6d831e8aa6fe01f33bfc4-0001.jp-tok.containers.appdomain.cloud
